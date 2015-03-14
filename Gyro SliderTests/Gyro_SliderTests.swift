cc09f7a6f84bcc876a163b160801507cc6bcdd65
Ben Scott <ben@schmurgon.net>
2015-03-15 10:10:56 +1100

    Initial Commit

---
+++ Gyro SliderTests/Gyro_SliderTests.swift
@@ -0,0 +1,36 @@
+//
+//  Gyro_SliderTests.swift
+//  Gyro SliderTests
+//
+//  Created by Ben Scott on 15/03/2015.
+//  Copyright (c) 2015 Schmurgon Pty Ltd. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Gyro_SliderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
