a12b2c833b2556a889288bf4b02afb4f062f30be
Ben Scott <ben@schmurgon.net>
2015-03-15 10:19:45 +1100

    Gyro Slider Demo
    First cut at a gyro demo using swift

--- Gyro Slider/ViewController.swift
+++ Gyro Slider/ViewController.swift
@@ -7,19 +7,56 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    let motionManager = CMMotionManager()
+    var updateTimer: NSTimer!
+    
+    @IBOutlet weak var rollSlider: UISlider!
+    
+    override func viewWillAppear(animated: Bool) {
+        rollSlider.minimumValue = -1.0
+        rollSlider.maximumValue = 1.0
+        
+        if motionManager.deviceMotionAvailable {
+            motionManager.startDeviceMotionUpdates()
+            startTimer()
+        }
     }
-
+    
+    override func viewWillDisappear(animated: Bool) {
+        if motionManager.deviceMotionAvailable {
+            motionManager.stopDeviceMotionUpdates()
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func startTimer() {
+        updateTimer = NSTimer.scheduledTimerWithTimeInterval(0.05,  target: self, selector: "updateSliderUsingRoll", userInfo: nil, repeats: true)
+    }
+    
+    func updateSliderUsingRoll() {
+        let motion = motionManager.deviceMotion
+        if motion != nil {
+            let roll = motion.attitude.roll
+            
+            dispatch_async(dispatch_get_main_queue(), {
+                var sliderValue = self.rollSlider.value
+                
+                if ( roll < 0 ) {
+                    sliderValue = sliderValue - 0.05;
+                } else {
+                    sliderValue = sliderValue + 0.05;
+                }
+                self.rollSlider.setValue( sliderValue, animated: true )
+            })
+        }
+    }
 }
 
